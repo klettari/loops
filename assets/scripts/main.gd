0cb762544c54c9793c8375793c15b093c8481b57
klettari <klettari.k@gmail.com>
2020-10-04 19:58:13 -0400

    LOOPS of all colors

--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -8,15 +8,15 @@ var spriteHeight = 124
 var screenWidth = 1024
 var screenHeight = 600
 
-var vericalOffset = 20
-var horizontalOffset = 100
+var vericalOffset = 100
+var horizontalOffset = 150
 
 var lowestXValue = spriteWidth / 2 + horizontalOffset
 var lowestYValue = spriteHeight / 2 + vericalOffset
 var highestXValue = screenWidth - 124 / 2 - horizontalOffset
 var highestYValue = screenHeight - 124 / 2 - vericalOffset
 
-var totalLoopsOfEachKind = 100
+var totalLoopsOfEachKind = 20
 
 func _ready():
 	random = RandomNumberGenerator.new()
@@ -24,10 +24,18 @@ func _ready():
 
 	var packagedSceneOrange = load("res://assets/scenes/orange.tscn")
 	var packagedSceneRed = load("res://assets/scenes/red.tscn")
+	var packagedSceneYellow = load("res://assets/scenes/yellow.tscn")
+	var packagedSceneGreen = load("res://assets/scenes/green.tscn")
+	var packagedSceneCyan = load("res://assets/scenes/cyan.tscn")
+	var packagedScenePurple = load("res://assets/scenes/purple.tscn")
 	
 	for i in range(0, totalLoopsOfEachKind) :
 		placeLoop(packagedSceneOrange)
 		placeLoop(packagedSceneRed)
+		placeLoop(packagedSceneYellow)
+		placeLoop(packagedSceneGreen)
+		placeLoop(packagedSceneCyan)
+		placeLoop(packagedScenePurple)
 
 func placeLoop(packagedScene) :
 	var x = random.randi_range(lowestXValue, highestXValue)
