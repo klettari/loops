861ee3e0efbb92eadb7e637cff9016d1634f4bd3
klettari <klettari.k@gmail.com>
2020-10-05 13:05:43 -0400

    better feeling movement control scheme for the rat

--- assets/scripts/rat.gd
+++ assets/scripts/rat.gd
@@ -8,12 +8,14 @@ var degreesLeft = 270
 var degreesStart = 0
 var degreesCircle = 360
 
-var degreesHalfwayBetween = 90 / 2
+var degreesDivisionSize = 3
+var degreesSlightliyForward = 90 / degreesDivisionSize
+var degreesAlmostThere = 90 / degreesDivisionSize * (degreesDivisionSize - 1)
 
-var degreesBetweenUpAndRight = degreesUp + degreesHalfwayBetween
-var degreesBetweenRightAndDown = degreesRight + degreesHalfwayBetween
-var degreesBetweenDownAndLeft = degreesDown + degreesHalfwayBetween
-var degreesBetweenLeftAndUp = degreesLeft + degreesHalfwayBetween
+var degreesBetweenUpAndRight = degreesUp + degreesAlmostThere
+var degreesBetweenRightAndDown = degreesRight + degreesSlightliyForward
+var degreesBetweenDownAndLeft = degreesDown + degreesAlmostThere
+var degreesBetweenLeftAndUp = degreesLeft + degreesSlightliyForward
 
 var rotationStep = 1
 var movementStep = 3
@@ -26,6 +28,11 @@ func _physics_process(delta) :
 
 	var betweenLeftAndUp = rotation_degrees >= degreesBetweenLeftAndUp and rotation_degrees < degreesCircle
 	var betweenUpAndRight = rotation_degrees >= degreesUp and rotation_degrees < degreesBetweenUpAndRight
+
+	if Input.is_action_pressed("left") :
+		rotation_degrees -= rotationStep
+	elif Input.is_action_pressed("right") :
+		rotation_degrees += rotationStep
 
 	if betweenLeftAndUp or betweenUpAndRight :
 		moveRatLookingUp()
@@ -45,37 +52,17 @@ func normalizedDegrees() :
 		rotation_degrees = degreesStart + degreesCircle - rotation_degrees
 
 func moveRatLookingDown() :
-	if Input.is_action_pressed("right") :
-		rotation_degrees -= rotationStep
-	elif Input.is_action_pressed("left") :
-		rotation_degrees += rotationStep
-
-	if Input.is_action_pressed("down") :
+	if Input.is_action_pressed("up") :
 		position += Vector2(0, movementStep)
 
 func moveRatLookingUp() :
-	if Input.is_action_pressed("right") :
-		rotation_degrees += rotationStep
-	elif Input.is_action_pressed("left") :
-		rotation_degrees -= rotationStep
-
 	if Input.is_action_pressed("up") :
 		position += Vector2(0, -movementStep)
 
 func moveRatLookingRight() :
-	if Input.is_action_pressed("down") :
-		rotation_degrees += rotationStep
-	elif Input.is_action_pressed("up") :
-		rotation_degrees -= rotationStep
-
-	if Input.is_action_pressed("right") :
+	if Input.is_action_pressed("up") :
 		position += Vector2(movementStep, 0)
 
 func moveRatLookingLeft() :
 	if Input.is_action_pressed("up") :
-		rotation_degrees += rotationStep
-	elif Input.is_action_pressed("down") :
-		rotation_degrees -= rotationStep
-
-	if Input.is_action_pressed("left") :
 		position += Vector2(-movementStep, 0)
